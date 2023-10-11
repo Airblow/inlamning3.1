690a303a706964dac44dfdac79ddd01597bbb10b
Airblow <143172177+Airblow@users.noreply.github.com>
2023-10-11 10:24:05 +0200

    Add project files.

---
+++ inlamning3.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace inlamning3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            short age = short.Parse(Console.ReadLine());
+
+            if (age >= 16 && age <= 19)
+                Console.WriteLine("Du får delta i tävlingen!");
+
+            else
+                Console.WriteLine("Du är inte i rätt ålder för tävlingen");
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
